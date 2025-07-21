18f9b55999bbad34a1d7a35da650e29316ae8ec5
Oleg Kazantsev <my.individual.address@gmail.com>
2025-07-21 18:06:36 +0900

    Попытка 7

--- features/editing/editing.php
+++ features/editing/editing.php
@@ -122,7 +122,6 @@ $parts_services = require '../../shared/parts.php';
 <html lang="ru">
     
     <?php
-        $ebitingCss = 'editing.css';
         include '../../shared/head.php';
     ?>
     <body>
