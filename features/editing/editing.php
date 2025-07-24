370fcc50fae3112a1d4f1afa073285a78a76fdc8
Oleg Kazantsev <my.individual.address@gmail.com>
2025-07-24 15:19:37 +0900

    [2025.07.24] Перенос файла database,attempt 5

--- features/editing/editing.php
+++ features/editing/editing.php
@@ -125,7 +125,7 @@ $parts_services = require __DIR__ . '/../../shared/parts.php';
             <h1> STANDOX </h1>
             <nav class="menu">
                 <ul>
-                    <li><a href="https://www.standox.pro/database/database.php" class="menu_link"> база данных </a></li>
+                    <li><a href="https://www.standox.pro/features/database/database.php" class="menu_link"> база данных </a></li>
                     <li><a href="https://www.standox.pro/logout.php" class="menu_link"> выйти </a></li>
                 </ul>
             </nav>
