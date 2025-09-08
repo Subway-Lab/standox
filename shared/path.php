761b62d85ab794cc20cb1846469e48716d18d5aa
Subway lab <my.individual.address@gmail.com>
2025-09-08 16:00:58 +0900

    2025.09.08 Synchronizing Main and Sandbox branches

--- shared/path.php
+++ shared/path.php
@@ -1,7 +1,13 @@
 <?php
 function getBasePath() {
     if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'standox.pro') !== false) {
-        $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+        $isHTTPS = (
+            (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
+            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||
+            (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)
+        );
+        
+        $protocol = $isHTTPS ? 'https' : 'http';
         return $protocol . '://www.standox.pro';
     }
     
